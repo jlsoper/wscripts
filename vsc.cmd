e29bc032e00b47b4131f2409b84ef7d2cfd12418
James <soapman@zoho.com>
2022-05-27 01:09:28 -0500

    Fri 05/27/2022  1:09:27.99 - LEOPARD-VM

--- vsc.cmd
+++ vsc.cmd
@@ -3,7 +3,7 @@
 
 GOTO //////////////////////////////////////////////////
 
-Visual Studio Code 1.51.1
+Visual Studio Code 1.67.2
 
 Usage: code.exe [options][paths...]
 
@@ -17,8 +17,6 @@ Options
   -n --new-window                   Force to open a new window.
   -r --reuse-window                 Force to open a file or folder in an
                                     already opened window.
-  --folder-uri <uri>                Opens a window with given folder uri(s)
-  --file-uri <uri>                  Opens a window with given file uri(s)
   -w --wait                         Wait for the files to be closed before
                                     returning.
   --locale <locale>                 The locale to use (e.g. en-US or zh-TW).
@@ -28,52 +26,54 @@ Options
   -h --help                         Print usage.
 
 Extensions Management
-  --extensions-dir <dir>
-      Set the root path for extensions.
-  --list-extensions
-      List the installed extensions.
-  --show-versions
-      Show versions of installed extensions, when using --list-extension.
-  --category
-      Filters installed extensions by provided category, when using --list-extension.
-  --install-extension <extension-id[@version] | path-to-vsix>
-      Installs or updates the extension. Use `--force` argument to avoid prompts. The identifier of an extension is always `${publisher}.${name}`. To install a specific version provide `@${version}`. For example: 'vscode.csharp@1.2.3'.
-  --uninstall-extension <extension-id>
-      Uninstalls an extension.
-  --enable-proposed-api <extension-id>
-      Enables proposed API features for extensions. Can receive one or more extension IDs to enable individually.
+  --extensions-dir <dir>              Set the root path for extensions.
+  --list-extensions                   List the installed extensions.
+  --show-versions                     Show versions of installed extensions,
+                                      when using --list-extensions.
+  --category <category>               Filters installed extensions by provided
+                                      category, when using --list-extensions.
+  --install-extension <ext-id | path> Installs or updates an extension. The
+                                      argument is either an extension id or a
+                                      path to a VSIX. The identifier of an
+                                      extension is '${publisher}.${name}'. Use
+                                      '--force' argument to update to latest
+                                      version. To install a specific version
+                                      provide '@${version}'. For example:
+                                      'vscode.csharp@1.2.3'.
+  --pre-release                       Installs the pre-release version of the
+                                      extension, when using
+                                      --install-extension
+  --uninstall-extension <ext-id>      Uninstalls an extension.
+  --enable-proposed-api <ext-id>      Enables proposed API features for
+                                      extensions. Can receive one or more
+                                      extension IDs to enable individually.
 
 Troubleshooting
-  -v --version                       Print version.
-  --verbose                          Print verbose output (implies --wait).
-  --log <level>                      Log level to use. Default is 'info'.
-                                     Allowed values are 'critical', 'error',
-                                     'warn', 'info', 'debug', 'trace', 'off'.
-  -s --status                        Print process usage and diagnostics
-                                     information.
-  --prof-startup                     Run CPU profiler during startup
-  --disable-extensions               Disable all installed extensions.
-  --disable-extension <extension-id> Disable an extension.
-  --sync <on> <off>                  Turn sync on or off
-  --inspect-extensions <port>        Allow debugging and profiling of
-                                     extensions. Check the developer tools for
-                                     the connection URI.
-  --inspect-brk-extensions <port>    Allow debugging and profiling of
-                                     extensions with the extension host being
-                                     paused after start. Check the developer
-                                     tools for the connection URI.
-  --disable-gpu                      Disable GPU hardware acceleration.
-  --max-memory                       Max memory size for a window (in Mbytes).
-  --telemetry                        Shows all telemetry events which VS code
-                                     collects.
+  -v --version                    Print version.
+  --verbose                       Print verbose output (implies --wait).
+  --log <level>                   Log level to use. Default is 'info'. Allowed
+                                  values are 'critical', 'error', 'warn',
+                                  'info', 'debug', 'trace', 'off'.
+  -s --status                     Print process usage and diagnostics
+                                  information.
+  --prof-startup                  Run CPU profiler during startup.
+  --disable-extensions            Disable all installed extensions.
+  --disable-extension <ext-id>    Disable an extension.
+  --sync <on | off>               Turn sync on or off.
+  --inspect-extensions <port>     Allow debugging and profiling of extensions.
+                                  Check the developer tools for the connection
+                                  URI.
+  --inspect-brk-extensions <port> Allow debugging and profiling of extensions
+                                  with the extension host being paused after
+                                  start. Check the developer tools for the
+                                  connection URI.
+  --disable-gpu                   Disable GPU hardware acceleration.
+  --max-memory <memory>           Max memory size for a window (in Mbytes).
+  --telemetry                     Shows all telemetry events which VS code
+                                  collects.
 
 
 ://////////////////////////////////////////////////
 
-IF DEFINED ZORRO (
-  C:\Apps\VSCodium\bin\codium --extensions-dir %USERPROFILE%\VSC\python\extensions --user-data-dir %USERPROFILE%\VSC\python\data
-) ELSE (
-  SET JAVA_HOME=
-  W:\VSCode\bin\code --extensions-dir %USERPROFILE%\VSC\java\extensions --user-data-dir %USERPROFILE%\VSC\java\data
-)
+W:\VSCode\bin\code --extensions-dir %USERPROFILE%\.vscode\extensions --user-data-dir %USERPROFILE%\.vscode\user-data
 
