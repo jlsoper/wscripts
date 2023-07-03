c51513460c547bcec23e8455c45d4e1f0e5c32d6
James <soapman@zoho.com>
2023-07-03 15:12:04 -0500

    Mon 07/03/2023 15:12:04.47 - LEOPARD-VM

---
+++ win-path.cmd
@@ -0,0 +1,28 @@
+@ECHO OFF
+
+GOTO //////////////////////////////////////////////////
+
+Displays or sets a search path for executable files.
+
+PATH [[drive:]path[;...][;%PATH%]
+PATH ;
+
+Type PATH ; to clear all search-path settings and direct cmd.exe to search
+only in the current directory.
+Type PATH without parameters to display the current path.
+Including %PATH% in the new path setting causes the old path to be
+appended to the new setting.
+
+::::::::::
+
+:: echo %Path:;=&echo.%
+
+%0 - script name
+%1 - 1st command line parameter
+%2 - 2nd command line parameter  (and so on till %9)
+%* - ALL command line parameters (excluding the script name itself)
+
+://////////////////////////////////////////////////
+
+echo.
+echo.%Path:;=&echo.%
