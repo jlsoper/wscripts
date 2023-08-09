d1e5472cbc38ff6849d19ca7a076b386eaac3fde
James <soapman@zoho.com>
2023-08-09 12:56:42 -0500

    Wed 08/09/2023 12:56:42.49 - LEOPARD2

--- sha512.cmd
+++ sha512.cmd
@@ -20,14 +20,14 @@ if ""%1""=="""" GOTO noPARAM
 setlocal
 
 set file1=%1
-set file2=%file1:~0,-4%
+set file2=%file1:~0,-7%
 
 :: echo %file1%
 :: echo %file2%
 
 for /F "tokens=1" %%g IN (%file1%) do set str1=%%g
 
-for /F "delims= " %%a in ('certUtil -hashfile %file2% md5 ^| findstr /v "hash"') do set str2=%%a
+for /F "delims= " %%a in ('certUtil -hashfile %file2% sha512 ^| findstr /v "hash"') do set str2=%%a
 
 :: echo %str1%
 :: echo %str2%
@@ -44,7 +44,7 @@ GOTO END
 
 :noPARAM
 
-ECHO "usage: %0 <file.md5>"
+ECHO "usage: %0 <file.sha512>"
 
 :END
 
